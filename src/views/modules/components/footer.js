92cf2e215986494131b5d3bb24302b992f7c900a
kj <kristoferjoseph@gmail.com>
2020-03-14 23:50:19 -0700

    Adds link to cache to footer.

--- src/views/modules/components/footer.js
+++ src/views/modules/components/footer.js
@@ -3,6 +3,8 @@ import { html } from '../vendor/preact.js'
 export default function Footer(props) {
   props = props || {}
   return html`
-<footer ...${props}></footer>
+<footer ...${props}>
+  <a href="/cache">Cache</a>
+</footer>
   `
 }
