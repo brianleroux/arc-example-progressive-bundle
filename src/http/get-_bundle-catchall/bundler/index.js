0eea183df108f9230a7c5dfa326ad55197da7c56
Brian LeRoux <b@brian.io>
2021-03-03 11:03:25 -0800

    smol fix for debug

--- src/http/get-_bundle-catchall/bundler/index.js
+++ src/http/get-_bundle-catchall/bundler/index.js
@@ -2,12 +2,12 @@ let sandbox = require('./sandbox')
 let s3 = require('./s3')
 
 /* returns a _static path to a file (eg. requesting /foo/bar.js returns /_static/foo/bar-3sd.js) */
-module.exports = async function bundler (file) {
+module.exports = async function bundler (meta) {
 
   // waterfall / debug flow
   if (process.env.ARC_BUNDLE === 'waterfall')
-    return `/_static/${file}`
+    return `/_static/${process.env.NODE_ENV === 'testing' ? meta : meta.file }`
 
   // otherwise return file from sandbox or s3
-  return process.env.NODE_ENV === 'testing' ? sandbox(file) : s3(file)
+  return process.env.NODE_ENV === 'testing' ? sandbox(meta) : s3(meta)
 }
